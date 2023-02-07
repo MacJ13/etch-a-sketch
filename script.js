352f145cd98ff2a2ad30f487a03323d4625acf1e
MacJ13 <jacdim02@gmail.com>
2023-02-08 00:08:57 +0100

    Add createGrid function
    Create given number of cell then
    we add them to grid element

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-const grid = document.querySelector('.grid');
+const gridElement = document.querySelector('.grid');
 
-const gridButton = document.querySelector('.btn-grid');
+const gridSizeButton = document.querySelector('.btn-grid');
 
 const colorButton = document.querySelectorAll ('.btn-color');
+
+function createGrid(size){
+    gridElement.innerHTML = ''
+    const number = size || 16;
+
+    gridElement.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+    gridElement.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+    gridElement.style.gap = '1px';
+
+    for(let i = 0; i< number*number; i++){
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        gridElement.appendChild(cell);
+    }
+}
+
+createGrid();
